96ce57261435adaf2f062b94605c700bf3663c5b
Arda Kaan Karakaş <62515456+ardakaankarakas@users.noreply.github.com>
2021-04-11 21:44:26 +0300

    Add files via upload

--- routes/floor.js
+++ routes/floor.js
@@ -2,6 +2,6 @@ const floors = require('../controllers/floorsController');
 const router = require('express').Router();
 
 router.post('/createFloor', floors.createFloor);
-router.post('/getFloor', floors.getFloor);
+router.get('/getFloor', floors.getFloor);
 
 module.exports = router;
